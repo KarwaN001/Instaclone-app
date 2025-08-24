d6ffffa0a837308d82755a333ddd2cd82bc8210d
KarwaN001 <karwanusf1@gmail.com>
2025-08-24 06:50:39 -0700

    Initial commit: InstaClone iOS project setup

---
+++ InstaClone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  InstaClone
+//
+//  Created by karwan Syborg on 24/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstaClone/InstaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCloneApp.swift
+//  InstaClone
+//
+//  Created by karwan Syborg on 24/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct InstaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
